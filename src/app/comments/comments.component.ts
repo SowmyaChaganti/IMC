65a9b21450432f8620d52703623e95e5041f428d
Astrid Sutton <astrid.sutton@wgu.edu>
2019-04-24 13:12:43 -0600

    testing for fb db

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'comments',
-  templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+	selector: 'comments',
+	templateUrl: './comments.component.html',
+	styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
+	items: Observable<any[]>;
 
-  constructor() { }
+	constructor(
+		db: AngularFirestore,
+	) {
+		this.items = db.collection('items').valueChanges();
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
